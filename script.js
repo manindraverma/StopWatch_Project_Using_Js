4e0b48d2a8e246d24febb055d38e237b7c64de4f
Manindra <manindra100@gmail.com>
2023-07-02 20:02:25 +0530

    added comments

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let minutes="00";
 let seconds="00";
 let miliseconds="00";
 
+//fetched all the html elements using queryselector
 let getHours=document.querySelector('.hours');
 let getMinutes=document.querySelector('.minutes');
 let getSeconds=document.querySelector('.seconds');
@@ -10,22 +11,35 @@ let getMiliseconds=document.querySelector('.miliseconds');
 let btnStart=document.querySelector('.btn-start');
 let btnStop=document.querySelector('.btn-stop');
 let btnReset=document.querySelector('.btn-reset');
+//creating a new identifier to store the setInterval id
 let interval;
 
+
+
+//pressing start button will invoke on click addeventListner function to start the stopWatch...
 btnStart.addEventListener('click',()=>{
+    //after first click on start button ,it is disabled
     btnStart.disabled=true;
+    //setInterval function is used to invoke the startTimer function after every 10ms and store the  Id as in interval variable returned from the setInterval
     interval=setInterval(startTimer,10);
 }
 )
 
+
+//pressing stop button will invoke on click addeventListner function to stop the stopWatch...
 btnStop.addEventListener('click',()=>{
+    //clearInterval function receives the interval id as argument and clear  the timer set with setinterval method as in above function
     clearInterval(interval);
+    //enable the start button as soon as we click on stop button
     btnStart.disabled=false;
 })
 
+//pressing reset button will invoke on click addeventListner function to reset  the stopWatch to inital state...
 btnReset.addEventListener('click',()=>{
     
+    //firstly we invoke clearInterval function and pass the interval variable 
     clearInterval(interval);
+    //enable the start button as soon as we click on reset button
     btnStart.disabled=false;
     miliseconds="00";
     seconds="00";
@@ -37,6 +51,8 @@ btnReset.addEventListener('click',()=>{
     getMiliseconds.innerHTML=miliseconds;
 })
 
+
+//startTimer function describes the working of the stopWatch
 function startTimer(){
     miliseconds++;
     if(miliseconds<=9){
